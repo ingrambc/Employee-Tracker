3b2f3673ce1df4d688699791352442dd31e32d69
Brian I <brianingram78@gmail.com>
2020-09-13 17:27:47 -0600

    added view by department, fixed view all manager issue

--- employeeManagement.js
+++ employeeManagement.js
@@ -23,12 +23,14 @@ const tasks = ["View All Employees", "View All Employees by Department", "View A
   "Add Employees", "Remove Employees", "Update Employee Role", "Update Employee Manager","add Role", "add Department", "Exit"];
 
 function start(){
+  //get input from user
   inquirer.prompt({
     name: "task",
     type: "list",
-    message: "What woul you like to do?",
+    message: "What would you like to do?",
     choices: tasks
   }).then(function(answer){
+    // call proper function from user choice
     switch (answer.task){
       case ("View All Employees"):
         viewAllEmps();
@@ -70,24 +72,54 @@ function start(){
 
 function viewAllEmps(){
   // Build querry statement
-  var querry = "SELECT a.id, a.first_name, a.last_name, c.name, b.title, b.salary ";
-  querry += "FROM employeeTbl a, roleTbl b, departmentTbl c ";
-  querry += "WHERE a.role_id = b.id AND b.department_id = c.id";
+  let querry = "SELECT a.id, a.first_name, a.last_name, b.name, c.title, c.salary, concat(d.first_name, ' ', d.last_name) AS manager ";
+      querry += "FROM employeetbl a ";
+      querry += "LEFT JOIN roletbl c ON a.role_id = c.id ";
+      querry += "LEFT JOIN departmenttbl b ON c.department_id = b.id ";
+      querry += "LEFT JOIN employeetbl d ON a.manager_id = d.id";
 
   // querry database, display results
   connection.query(querry, function(err, res){
-    if(err)throw err;
-    console.log(res);
+    if(err) throw err;
     console.table(res);
     start();
   })
-  
-}
+}//end of function
 
 function viewByDept(){
-  console.log("Entered viewByDept");
-  start();
-}
+  //get department list
+  connection.query("SELECT DISTINCT name FROM departmentTbl", function(err, res){
+    if(err) throw err;
+    //turn response into list
+    let departments = [];
+    for (let i = 0; i < res.length; i++) {
+      departments.push(res[i].name);      
+    }
+
+    //get department to view
+    inquirer.prompt({
+      name: "department",
+      type: "list",
+      message: "What department would you lke to view?",
+      choices: departments
+    }).then(function(answer){
+      //build querry statement
+      let querry = "SELECT a.id, a.first_name, a.last_name, b.name, c.title, c.salary, concat(d.first_name, ' ', d.last_name) AS manager ";
+      querry += "FROM employeetbl a ";
+      querry += "LEFT JOIN roletbl c ON a.role_id = c.id ";
+      querry += "LEFT JOIN departmenttbl b ON c.department_id = b.id ";
+      querry += "LEFT JOIN employeetbl d ON a.manager_id = d.id ";
+      querry += "WHERE b.name = ?";
+
+      //run querry and display result
+      connection.query(querry, [answer.department], function(err, res){
+        if(err) throw err;
+        console.table(res);
+        start();
+      })
+    })
+  })
+}//end of function
 
 function viewByMan(){
   console.log("Entered viewByMan");
